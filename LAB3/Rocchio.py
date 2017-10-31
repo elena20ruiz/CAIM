412ee062906d3204e1da760ca5768c925ea80a6a
elena20ruiz <elena.ruiz.bdn@gmail.com>
2017-10-31 07:44:52 +0100

    Update rocchio

--- LAB3/Rocchio.py
+++ LAB3/Rocchio.py
@@ -30,6 +30,45 @@ from elasticsearch_dsl.query import Q
 
 __author__ = 'bejar'
 
+
+"""
+FUNCIONS LABS ANTERIORS
+"""
+
+def normalize(tw):
+    """
+    Normalizes the weights in t so that they form a unit-length vector
+    It is assumed that not all weights are 0
+    :param tw:
+    :return:
+    """
+    suma = 0
+    for _,x in tw:
+        suma += x
+    return [float(w)/suma for _,w in tw]
+
+def toTFIDF(client, index, file_id):
+    """
+    Returns the term weights of a document
+    :param file:
+    :return:
+    """
+    # Get document terms frequency and overall terms document frequency
+    file_tv, file_df = document_term_vector(client, index, file_id)
+    max_freq = max([f for _, f in file_tv])
+    dcount = doc_count(client, index)
+
+    tfidfw = []
+    for (t, w),(_, df) in zip(file_tv, file_df):
+        # 1. Calculo tfid: nombre freq del doc entre freq total
+        tfid = w / max_freq
+        # 2. Calculo idfi (inversa freq del doc sobre el term i)
+        idfi = math.log(dcount / df,2)
+        weight = tfid * idfi
+        tfidfw.append([t,weight])
+        pass
+    return normalize(tfidfw)
+
 def RocchioRule(alpha,query,beta,mean):
     """
     NOU CODI
@@ -42,19 +81,19 @@ def RocchioRule(alpha,query,beta,mean):
             dquery.append(word,1)
         else:
             s = word.split("^")
-            dquery.append(s[0],s[1])
+            dquery.append([s[0],s[1]])
 
     #2. Calcular nous pesos
     newquery= {}
     for q in dquery:
         w = alpha*q + beta*mean
-        newquery.append(q.key,w)
+        newquery.append([q.key,w])
 
     #TODO: 3. Passar un altre cop de diccionari a query
     newq = ""
     for nq in newquery:
         newq.join(nq[0],"^",nq[1]," ")
-    return newqu
+    return newq
 
 
 
@@ -86,20 +125,29 @@ if __name__ == '__main__':
         s = Search(using=client, index=index)
 
         if query is not None:
-            for i in range(0,nrounds):
-                #TODO: Calcular MEAN
-                query = RocchioRule(alpha,query,beta,mean)  #Retornem nova query
-                q = Q('query_string',query=query[0])
-                for i in range(1, len(query)):
-                    q &= Q('query_string',query=query[i])
+            #FIRST ITERATION: NO ROCCHIO RULE
+            q = Q('query_string',query=query[0])
+            for i in range(1, len(query)):
+                q &= Q('query_string',query=query[i])
 
+            s = s.query(q)
+            docsit = s[0:nhits].execute()
+            #START NEW ROUNDS APPLYIN ROCCHIO RULE
+            for i in range(0,nrounds):
+                #TODO: Calcular MEAN a partir de docsit
+
+                q = RocchioRule(alpha,q,beta,mean)  #Retornem nova query
+                #EXECUTEM PER A LA SEGÜENT ITERACIÓ
                 s = s.query(q)
-                response = s[0:nhits].execute()
-                for r in response:  # only returns a specific number of results
-                    print('ID= %s SCORE=%s' % (r.meta.id,  r.meta.score))
-                    print('PATH= %s' % r.path)
-                    print('TEXT: %s' % r.text[:50])
-                    print('-----------------------------------------------------------------')
+                docsit = s[0:nhits].execute()
+
+            #ORDEN FINAL
+            response = s[0:nhits].execute()
+            for r in response:  # only returns a specific number of results
+                print('ID= %s SCORE=%s' % (r.meta.id,  r.meta.score))
+                print('PATH= %s' % r.path)
+                print('TEXT: %s' % r.text[:50])
+                print('-----------------------------------------------------------------')
         else:
             print('No query parameters passed')
 
