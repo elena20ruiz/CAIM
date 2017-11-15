f9ce430496dcad39db01ecdf1a9f21c1448d8c1e
elena.ruiz <elena.ruiz@upcnet.es>
2017-11-15 17:03:02 +0100

    page rank

--- LAB4/PageRank.py
+++ LAB4/PageRank.py
@@ -5,14 +5,13 @@ import time
 import sys
 
 class Edge:
-    def __init__ (self, origin=None):
-        self.origin = ... # write appropriate value
-        self.weight = ... # write appropriate value
+    def __init__ (self, origin=None, dest = None):
+        self.origin = origin
+        self.dest = dest
+        self.weight = 0
 
     def __repr__(self):
-        return "edge: {0} {1}".format(self.origin, self.weight)
-        
-    ## write rest of code that you need for this class
+        return "edge: {0} {1} {2}".format(self.origin, self.dest, self.weight)
 
 class Airport:
     def __init__ (self, iden=None, name=None):
@@ -29,6 +28,19 @@ edgeList = [] # list of Edge
 edgeHash = dict() # hash of edge to ease the match
 airportList = [] # list of Airport
 airportHash = dict() # hash key IATA code -> Airport
+
+
+## AIRPORT FILE:
+## [0]: Id OpenFlights airport. :: 1
+## [1]: name of the airport.    :: "Goroka"          ----------SE UTILIZA
+## [2]: City of airport         :: "Goroka"
+## [3]: Country                 :: "Papua New Guinea"----------SE UTILIZA
+## [4]: IATA CODE (3 letters)   :: "GKA"             ----------SE UTILIZA
+## [5]: OCAO code (4 letters)   :: "AYGA"
+
+##CLASS AIRPORT
+## a.name = [1], [3] = "name airport", "Country"
+## a.code = [4] = "IATA code"
 
 def readAirports(fd):
     print "Reading Airport file from {0}".format(fd)
@@ -51,10 +63,38 @@ def readAirports(fd):
     airportsTxt.close()
     print "There were {0} Airports with IATA code".format(cont)
 
+## ROUTES FILE:
+## [0]: airline code.           :: 2B
+## [1]: OpenFlights airline code:: 410
+## [2]: Origin IATA o ICAO code :: AER                --------SE UTILIZA
+## [3]: Destin IATA o ICAO code :: "Papua New Guinea" --------SE UTILIZA
 
 def readRoutes(fd):
     print "Reading Routes file from {0}".format(fd)
-    # write your code
+    routesTxt = open(fd, "r");
+    cont = 0
+    for line in routesTxt.readlines():
+
+        try:
+            temp = line.split(',')
+            if len(temp[4]) != 5 :
+                raise Exception('not an IATA code')
+            ## Si no se ha añadido aun aeropuerto destino en origen
+            code_or = temp[2][1:-1]     #IATA origen
+            code_dest = temp[3][1:-1]   #IATA destino
+
+            if edgeHash[code_or] == null && edgeHash[code_dest]
+            ## BUSCAR aeropuerto POR IATA
+            a = airportHash[code_or]
+            a.routes.append()
+
+        except Exception as inst:
+            pass
+        else:
+            cont += 1
+            airportList.append(a)
+            airportHash[a.code] = a
+    airportsTxt.close()
 
 def computePageRanks():
     # write your code
